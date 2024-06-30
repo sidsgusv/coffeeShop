9c031ef0cbeeced7858c7321a5b598fdcc85e63f
sidsgusv <95411237+sidsgusv@users.noreply.github.com>
2024-06-30 07:54:21 +0300

    Initial commit

---
+++ src/main/java/com/example/coffeeshopapl/CoffeeShopAplApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeshopapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopAplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeShopAplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coffeeshopapl/CoffeeShopAplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeshopapl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopAplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
